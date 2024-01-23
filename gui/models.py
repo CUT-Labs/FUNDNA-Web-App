4fd96fc32734f773f36fb03cf9e7eb8380a79cc4
Jacksonnnnn <jhuse01@gmail.com>
2024-01-23 14:29:21 -0500

    begin gui development

---
+++ gui/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Function():
+    pass
+
+
+class CRN():
+    pass
+
+
+class Gate():
+    pass
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("gui/", include("gui.urls")),
     path('admin/', admin.site.urls),
 ]
