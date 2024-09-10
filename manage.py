af1c6f9f39b670c41a68103747c1b87bd3592d34
Jacksonnnnn <jhuse01@gmail.com>
2024-09-10 17:38:24 -0400

    temporarily disable all piperine issues, force django server to run on 16458

--- manage.py
+++ manage.py
@@ -6,6 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
+
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "16458"
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
