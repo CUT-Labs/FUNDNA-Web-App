38c57563f624ed508dd75d9e0a69a1e1586f7ade
Jacksonnnnn <jhuse01@gmail.com>
2024-09-11 15:28:28 -0400

    initial setup with basic home page

--- gui/views.py
+++ gui/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')  # This will extend from base.html
+
+# def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("gui/", include("gui.urls")),
+    path("", include("gui.urls")),  # Include gui app URLs
     path('admin/', admin.site.urls),
 ]
